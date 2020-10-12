d97d915cca4f75cb0b8b9b217203b2ba7bab6574
Adao Natalino <adaojosen@hotmail.com>
2020-10-12 18:05:28 +0100

    VS

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -2,14 +2,39 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Honeywell_backend.Data;
+using Honeywell_backend.Models;
+using Honeywell_backend.Serializers;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Honeywell_backend.Controllers
-{
-    [Route("api/[controller]")]
+namespace Honeywell_backend.Controllers {
+    [Route ("api/[controller]")]
     [ApiController]
-    public class UsersController : ControllerBase
-    {
+    public class UsersController : ControllerBase {
+        private HoneywellDB _context;
+
+        public UsersController (HoneywellDB context) {
+            _context = context;
+        }
+
+        [HttpPost]
+
+        public async Task<ActionResult> CreateUser ([FromBody] UserSerializer request) {
+            if (!ModelState.IsValid) {
+                var errorList = ModelState.Values.SelectMany (e => e.Errors).ToList ();
+                var errormsgs = new List<string> ();
+
+                errorList.ForEach (e => {
+                    errormsgs.Add (e.ErrorMessage);
+                });
+                var result = new { sucess = false, errors = errormsgs };
+                BadRequest (result);
+            }
+
+            var clientDb = await _context.Users
+                                 .FirstOrDefaultAsync(c => c.Username == request.Username);
+            return Ok ("ok");
+        }
     }
 }
--- Serializers/UserSerializer.cs
+++ Serializers/UserSerializer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,14 +8,32 @@ namespace Honeywell_backend.Serializers
 {
     public class UserSerializer
     {
-
         public int Id { get; set; }
+        
+        [Required]
+        [MaxLength(100)]
         public string Name { get; set; }
+        
+        [Required]
+        [MaxLength(100)]
         public string Address { get; set; }
+        
+        [Required]
         public int Phone { get; set; }
+        
+        [Required]
+        [MaxLength(100)]
         public string Email { get; set; }
-        public string Username { get; set; }               
+        
+        [Required]
+        [MaxLength(100)]
+        public string Username { get; set; }    
+        
+        [Required]
+        [MaxLength(100)]           
         public string Password { get; set; }
+        
+        [Required]
         public bool IsStaff { get; set; }
     }
 }
